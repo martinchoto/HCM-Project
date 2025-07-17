2cf833a4e59669f4e3caffc4d12825ea97ceb036
martinchoto <martin_stalev03@abv.bg>
2025-07-17 15:08:28 +0300

    git init

--- Human Capital Management/Human Capital Management/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Human_Capital_Management.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
