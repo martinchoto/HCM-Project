1c52de5bd930f330708ef3805312f680117b0315
martinchoto <martin_stalev03@abv.bg>
2025-07-17 15:35:43 +0300

    created db schema

---
+++ HumanCapitalManagement/HumanCapitalManagement/Data/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HumanCapitalManagement.Data
+{
+	public class Department
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; } = null!;
+		public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+		
+	}
+}
---
+++ HumanCapitalManagement/HumanCapitalManagement/Data/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace HumanCapitalManagement.Data
+{
+	public class Employee
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string FirstName { get; set; } = null!;
+		[Required]
+		public string LastName { get; set; } = null!;
+		[Required]
+		public string Email { get; set; } = null!;
+		[Required]
+		public string JobTitle { get; set; } = null!;
+		public decimal Salary { get; set; }
+		[ForeignKey(nameof(Department))]
+		public int DepartmentId { get; set; }
+		public Department Department { get; set; } = null!;
+	}
+}
